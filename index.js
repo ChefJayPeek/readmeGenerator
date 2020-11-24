39411899d52286fd386b775b185ed9d2dd2083e8
Jay Peek <73311799+ChefJayPeek@users.noreply.github.com>
2020-11-24 15:15:36 -0600

    initial commit with some psuedo code.

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+
+// Generate the sections of the Readme.md for : Title of Project, Description, Table of Contents, Installation, Usagfe, licensing, Contributors, Tests and Questions
+
+// Get Title of Project
+// Get Description of Project
+// Get Installation Instructions
+// Get Usage Information
+// Get Contributors
+// Get Tests
+// Get License infromation from a list of options
+// Get GitHub username
+// get email address
+
